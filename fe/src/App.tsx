8c35dd959eaf19d55f27f7f26d95f179fbbf863f
Yan Weschenfelder Rodrigues <yanywr@gmail.com>
2021-05-04 15:38:14 -0300

    #4 - Static charts

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,12 +1,28 @@
 import HeaderComponent from "components/Header";
 import DataTableComponent from "components/DataTable";
 import FooterComponent from "components/Footer";
+import BarChartComponent from "components/Charts/Bar";
+import DonutChartComponent from "components/Charts/Donut";
 
 function App() {
   return (
     <>
       <HeaderComponent />
       <div className="container">
+        <h1 className="text-primary py-3">DashBoard</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChartComponent />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <DonutChartComponent />
+          </div>
+        </div>
+
+        <h1 className="text-primary py-3">Todas vendas</h1>
         <DataTableComponent />
       </div>
       <FooterComponent />
---
+++ fe/src/components/Charts/Bar/index.tsx
@@ -0,0 +1,35 @@
+import Chart from 'react-apexcharts';
+
+export default function BarChartComponent() {
+  const options = {
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      }
+    },
+  };
+
+  const mockData = {
+    labels: {
+      categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    },
+    series: [
+      {
+        name: "% Sucesso",
+        data: [43.6, 67.1, 67.7, 45.6, 71.1]
+      }
+    ]
+  };
+
+  return (
+    <Chart
+      options={{
+        ...options,
+        xaxis: mockData.labels
+      }}
+      series={mockData.series}
+      type="bar"
+      height="240"
+    />
+  );
+}
