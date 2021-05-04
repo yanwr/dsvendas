0ccb953822df32d8945e13e90b029f7d8c7db2ad
Yan Weschenfelder Rodrigues <yanywr@gmail.com>
2021-05-04 13:47:52 -0300

    #0 - Project created

---
+++ be/src/main/java/com/projects/dsvendas/DsvendasApplication.java
@@ -0,0 +1,11 @@
+package com.projects.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+}
---
+++ be/src/test/java/com/projects/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
